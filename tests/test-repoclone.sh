53622bb97e5dd60e774fcb6c4a558a045f5d844a
Dan Liu <nad2040@gmail.com>
2025-09-11 23:44:48 -0400

    add github repo clone as a test

---
+++ tests/test-repoclone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+PROJ_ROOT="${PROJ_ROOT:-$(git rev-parse --show-toplevel)}"
+TEST="${PROJ_ROOT}"/tests
+TRACE=${TRACE:-$1}
+
+mkdir -p "${TEST}"/repoclone
+cd "${TEST}"/repoclone
+
+"${TRACE}" sh -c "git clone https://github.com/git/git.git"
+
+rm -rf "${TEST}"/repoclone/*
+rmdir "${TEST}"/repoclone
