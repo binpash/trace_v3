29bedb54685fdd1eb9335df77120862e14928d29
Divik Chotani <divik.chotani@gmail.com>
2026-03-19 12:00:00 -0700

    created man page

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ set -x
 export PROJ_ROOT="$(git rev-parse --show-toplevel)"
 
 sudo install -o root -m 4755 "${PROJ_ROOT}/target/release/trace_v3" /usr/local/bin/
+
+sudo mkdir -p /usr/local/share/man/man1
+sudo install -o root -m 0644 "${PROJ_ROOT}/man/trace_v3.1" /usr/local/share/man/man1/trace_v3.1
+
+sudo mandb
