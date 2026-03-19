90a589edc24ba33cba7b3e861e4a6998cdf09cf1
Divik Chotani <divik.chotani@gmail.com>
2026-03-19 12:00:00 -0700

    created man page

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ export PROJ_ROOT="$(git rev-parse --show-toplevel)"
 
 cargo build --release
 sudo install -o root -m 4755 "${PROJ_ROOT}/target/release/trace_v3" /usr/local/bin/
+
+sudo mkdir -p /usr/local/share/man/man1
+sudo install -o root -m 0644 "${PROJ_ROOT}/man/trace_v3.1" /usr/local/share/man/man1/trace_v3.1
+
+sudo mandb
