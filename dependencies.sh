6eddce3764bc80f3143a0e68e4c8d4838afd6283
Dan Liu <nad2040@gmail.com>
2026-05-16 19:14:59 -0400

    fix dependency script

--- dependencies.sh
+++ dependencies.sh
@@ -2,12 +2,9 @@
 
 set -ex
 
-
-
-
 setup_apt() {
-    sudo apt install gcc clang llvm libbpf-dev zlib1g-dev libelf-dev autopoint flex bison
+    sudo apt update
+    sudo apt install -y gcc clang llvm libbpf-dev zlib1g-dev libelf-dev autopoint flex bison pkg-config
 }
-
 
 setup_apt
